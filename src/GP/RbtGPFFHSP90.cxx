062b61f393c299529b2eb717a9564eeff5fd062b
pschmidtke <pschmidtke@119cc886-e0c5-4eb3-b525-b0b2daa712b1>
2012-07-01 21:36:50 +0000

    license change

--- src/GP/RbtGPFFHSP90.cxx
+++ src/GP/RbtGPFFHSP90.cxx
@@ -1,9 +1,17 @@
-/***********************************************************************
-* $Id: //depot/dev/client3/rdock/2006.1/src/GP/RbtGPFFHSP90.cxx#3 $
-* Copyright (C) Vernalis (R&D) Ltd 2006
-* This file is released under the terms of the End User License Agreement
-* in ../../docs/EULA.txt
-***********************************************************************/
+/*This file is part of Rdock.
+
+    Rdock is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Rdock is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Rdock.  If not, see <http://www.gnu.org/licenses/>.*/
 
 #include "RbtGPFFHSP90.h"
 #include "RbtGPGenome.h"
--- src/exe/smart_rms.cxx
+++ src/exe/smart_rms.cxx
@@ -1,5 +1,17 @@
-//Ancillary Software from Vernalis
-//$Id: //depot/dev/client3/rdock/2006.1/src/exe/smart_rms.cxx#5 $
+/*This file is part of Rdock.
+
+    Rdock is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Rdock is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Rdock.  If not, see <http://www.gnu.org/licenses/>.*/
 
 //Calculates atomic RMSD of each SD record with reference structure
 //Requires Daylight SMARTS toolkit to check all symmetry-related atomic numbering paths
