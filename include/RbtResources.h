f8ccd3fe06b3e84697a5a2449855406806920ac9
Vedran Miletić <vedran@miletic.net>
2019-05-08 21:16:48 +0200

    Replaced no-break space (\xC2\xA0) with space (\x20)

--- include/RbtResources.h
+++ include/RbtResources.h
@@ -1,11 +1,11 @@
 /***********************************************************************
-* The rDock program was developed from 1998 - 2006 by the software team 
+* The rDock program was developed from 1998 - 2006 by the software team 
 * at RiboTargets (subsequently Vernalis (R&D) Ltd).
-* In 2006, the software was licensed to the University of York for 
+* In 2006, the software was licensed to the University of York for 
 * maintenance and distribution.
 * In 2012, Vernalis and the University of York agreed to release the 
 * program as Open Source software.
-* This version is licensed under GNU-LGPL version 3.0 with support from
+* This version is licensed under GNU-LGPL version 3.0 with support from
 * the University of Barcelona.
 * http://rdock.sourceforge.net/
 ***********************************************************************/
@@ -18,7 +18,7 @@
 #include "RbtTypes.h"
 #include "VERSION"
 
-const RbtString IDS_COPYRIGHT = "The rDock program is licensed under GNU-LGPLv3.0. http://rdock.sourceforge.net/";
+const RbtString IDS_COPYRIGHT = "The rDock program is licensed under GNU-LGPLv3.0. http://rdock.sourceforge.net/";
 #ifdef _DEBUG
 const RbtString IDS_PRODUCT   = "libRbtD.so";
 #else
