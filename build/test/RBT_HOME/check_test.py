db7745e496f7897d42a11949cbca898a8db88b55
Vedran Miletić <vedran@miletic.net>
2019-07-23 18:15:44 +0200

    Changed testing to run rbdock after unit tests
    Cleaned up test checker Python script and made it compatible with
    Python 3, but decided not to use it because the results (that depend
    on a random number generator) weren't similar enough.

--- build/test/RBT_HOME/check_test.py
+++ build/test/RBT_HOME/check_test.py
@@ -1,7 +1,16 @@
 import sys
 
-ref=[item2.split() for item2 in [item.rstrip() for item in open(sys.argv[1]).readlines()]]
-test=[item2.split() for item2 in [item.rstrip() for item in open(sys.argv[2]).readlines()]]
+TOLERANCE = 0.01
+
+try:
+	ref=[item2.split() for item2 in [item.rstrip() for item in open(sys.argv[1]).readlines()]]
+	test=[item2.split() for item2 in [item.rstrip() for item in open(sys.argv[2]).readlines()]]
+except IndexError:
+	print("Need to specify two file names.")
+	sys.exit(2)
+except FileNotFoundError:
+	print("Specified file(s) not found.")
+	sys.exit(1)
 
 refcoords=ref[4:48]
 testcoords=test[4:48]
@@ -13,14 +22,14 @@ checkcoords=[]
 for i in range(0,len(refcoords)):
 	for j in [0,1,2]:
 		checkcoords.append(abs(float(refcoords[i][j])-float(testcoords[i][j])))
-error=0
+error = False
 for check in checkcoords:
-	if check > 0.01:
-		error=1
-if abs(refscore - testscore) > 0.01:
-	error = 1
+	if check > TOLERANCE:
+		error = True
+if abs(refscore - testscore) > TOLERANCE:
+	error = True
 
-if error == 1:
-	print "The test failed, please check the compilation is OK and no errors were raised."
+if error:
+	print("The test failed, please check the compilation is OK and no errors were raised.")
 else:
-	print "The test succeeded! The results agree with the reference ones.\nHave fun using rDock!!"
+	print("The test succeeded! The results agree with the reference ones.\nHave fun using rDock!!")
